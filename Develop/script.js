7ace06d99245ed5d4dca19bcad86e410a294e422
SpectralRaiden <subliminality.reality@gmail.com>
2022-12-19 18:56:34 -0500

    created generatePassword function along w/ varible arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,18 @@
 // Assignment code here
 
 
+function generatePassword() {
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const spChars = ['!', '@', '#', '%', '^', '&', '*', '(', ')', '_', '~', '?'];
+  const alphs = Array.from(Array(26)).map((e, i) => i + 65);
+  const upAlphs = alphs.map((x) => String.fromCharCode(x));
+  const lowAlphs = upAlphs.map(letter => letter.toLowerCase());
+  
+  console.log(lowAlphs);
+
+  return 'PLACEHOLDER TEXT';
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
